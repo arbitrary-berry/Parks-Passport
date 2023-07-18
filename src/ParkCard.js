ca689a455a6b2349fb750e9568abc969c6c89f98
arbitrary-berry <alyssa.brooksong@gmail.com>
2023-07-18 11:45:26 -0700

    Park Card semantic ui

--- src/ParkCard.js
+++ src/ParkCard.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Card } from 'semantic-ui-react';
+import { AiOutlineHeart } from 'react-icons/ai';
 
-
-function ParkCard({Photo, LocationName}) {
-    return (
-        <div className="card">
-            <img src={Photo} alt={LocationName}/>
-            <h2>{LocationName}</h2>
-        </div>
-    )
-}
+const extra = (
+    <a>
+      <AiOutlineHeart />
+    </a>
+  )
+  
+  const ParkCard = ({ Photo, LocationName, Tagline}) => (
+    <Card
+      image={Photo}
+      extra={extra}
+      header={LocationName}
+      description={Tagline}
+    />
+  )
 
 export default ParkCard
