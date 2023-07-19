d860c90039dd5adef36aa37d176c07cbf657f267
Alyssa <alyssa.brooksong@gmail.com>
2023-07-19 16:02:27 -0700

    reviews posted to db and render on page

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReviewForm from "./ReviewForm"
 import WishList from "./WishList"
+import ReviewContainer from "./ReviewContainer"
 
 
 function Profile() {
@@ -13,14 +14,21 @@ function Profile() {
             .then((parks) => setParks(parks))
     }, [])
 
+    useEffect(() => {
+        fetch("http://localhost:3000/reviews")
+            .then(res => res.json())
+            .then((reviews) => setReviews(reviews))
+    }, [])
+
     function addNewReview(newReview) {
-        setParks([...parks, newReview])
+        setReviews([...reviews, newReview])
     }
 
 
     return (
         <div>
-            <ReviewForm parks={parks} reviews={reviews} addNewReview={addNewReview}/>
+            <ReviewForm parks={parks} addNewReview={addNewReview}/>
+            <ReviewContainer reviews={reviews} />
             <WishList />
         </div>
 
